ffef08c3b58cdc5a74e5fd008373ffbcc27e39e2
brian andrich <brianandrich@gmail.com>
2012-11-28 22:02:02 -0500

    dfh

--- js/main.js
+++ js/main.js
@@ -1,6 +1,40 @@
 $(document).ready(function(){
-
+	
+	$('.loading p').fadeIn(1200);
+	
+	setTimeout(start,1600);
+	
 	$("a[rel=tooltip]").tooltip();
 	$(".carousel").carousel();
+	$('body').on('slid', function() {
+		var current = $('.circles span.active');
+		var slide = $(".carousel .item.active").index();
+		var next = $('.circles span').eq(slide);
+		current.removeClass('active');
+		next.addClass('active');
+	});
+	
+	$('.circles span').click(function() {
+		var x = $(this).index();
+		$(".carousel").carousel(x);
+	});	
+	
+	$(".product img").hover(
+		function() {
+			var x = $(this).parents('.product').find('h4');
+			x.toggleClass('under');
+		}, 
+		function() {
+			var x = $(this).parents('.product').find('h4');
+			x.toggleClass('under');
+		}
+	);
 	
 });
+
+var start = function() {
+	$(".loading").fadeOut(450, function(){
+		$('.app').fadeIn(450)
+	})
+}
+
